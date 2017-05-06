7876d7a0c5ef823358ce718679b177faab29ad9e
rafgardom <rafgardom2@alum.us.es>
2017-05-06 20:35:53 +0200

    Corrección en la generación de facturas

--- Acme-Chorbies/src/main/java/repositories/InvoiceRepository.java
+++ Acme-Chorbies/src/main/java/repositories/InvoiceRepository.java
@@ -16,7 +16,7 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
 	@Query("select i from Invoice i where i.openPeriod = ?1 and i.endPeriod = ?2 and i.chorbi.id = ?3")
 	public Invoice findInvoiceByParams(Date openPeriod, Date endPeriod, int chorbiId);
 
-	@Query("select i from Invoice i where i.openPeriod = (select min(i2.openPeriod) from Invoice i2 where i2.chorbi.id = ?1) and i.chorbi.id = ?1")
+	@Query("select i from Invoice i where i.openPeriod = (select max(i2.openPeriod) from Invoice i2 where i2.chorbi.id = ?1) and i.chorbi.id = ?1")
 	public Invoice findLastInvoiceByChorbi(int chorbiId);
 
 	@Query("select i from Invoice i where i.openPeriod > ?1 and i.endPeriod < ?2")
